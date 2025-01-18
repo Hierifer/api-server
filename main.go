cd273ef3e7f0c94bbed74e7a78a01ddb216a5d5b
Hierifer <hierifer@hotmail.com>
2025-01-18 19:35:46 +0800

    feat: [swagger] add swagger

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	v1 "api-server/api/v1" // Ensure this path is correct and the module exists
+
 	"github.com/gin-gonic/gin"
-	v1 "github.com/hierifer/api-server/api/v1" // Ensure this path is correct and the module exists
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	_ "github.com/swaggo/gin-swagger/example/multiple/docs"
@@ -17,7 +18,7 @@ func main() {
 
 	// Register api/v1 endpoints
 	v1.Register(r)
-	r.GET("/v1/*any", ginSwagger.WrapHandler(swaggerfiles.NewHandler(), ginSwagger.InstanceName("v1")))
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.NewHandler(), ginSwagger.InstanceName("v1")))
 
 
 	_ = r.Run() // listen and serve on 0.0.0.0:8080
